3543b6405ea0ad7deb01b894d130600094cb7d60
Tri <anhtri54@gmail.com>
2024-09-11 02:33:38 +0200

    asd

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,11 +1,42 @@
 const todoList = [];
 
+let todoListHTML = '';
+renderToDo()
+function renderToDo(){
+
+  todoListHTML = '';
+
+  for(let i = 0; i < todoList.length; i++){
+    const todoObject = todoList[i];
+    const {name, dueDate} = todoObject
+    todoListHTML += `
+    <div> ${name}</div>
+    <div> ${dueDate} </div>
+    <button onclick="
+    todoList.splice(${i}, 1);
+    renderToDo();
+    "class="deleteButton">Delete</button>
+    </p>`;
+   }
+   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+
 function addToDo(){
  const inputElement = document.querySelector('.js-name-input');
- const name = inputElement.value;
- todoList.push(name);
+ const name = inputElement.value
+
+const dateInputElement = document.querySelector('.js-due-date-input');
+const dueDate = dateInputElement.value;
+
+
+ todoList.push({
+  name, dueDate
+
+ });
  console.log(todoList);
 
-
  inputElement.value = '';
+ renderToDo();
+
 }
